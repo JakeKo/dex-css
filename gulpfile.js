330e6891b2bd9dd95cc00ebdc0850106ec31c5fc
Jacob Koperski <wijakoperski@gmail.com>
2017-12-30 23:18:52 -0600

    Added cssnano for minifcation and a minify gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,19 @@ var gulp = require('gulp');
 var stylus = require('gulp-stylus');
 var postcss = require('gulp-postcss');
 var autoprefixer = require('autoprefixer');
+var cssnano = require('cssnano');
  
 gulp.task('css', function () {
 	return gulp.src('./css/*.styl')
 		.pipe(stylus())
 		.pipe(postcss([autoprefixer]))
+		.pipe(gulp.dest('./dist/css/'));
+});
+
+gulp.task('minify', function() {
+	return gulp.src('./css/*.styl')
+		.pipe(stylus())
+		.pipe(postcss([autoprefixer, cssnano]))
 		.pipe(gulp.dest('./dist/css/'));
 });
 
