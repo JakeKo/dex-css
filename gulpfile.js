9fc98fce13910195b83f18788b5f5a8994ad2cb1
Jacob Koperski <wijakoperski@gmail.com>
2017-12-30 23:06:11 -0600

    Minor abstraction of the css task

--- gulpfile.js
+++ gulpfile.js
@@ -3,9 +3,11 @@ var stylus = require('gulp-stylus');
 var postcss = require('gulp-postcss');
 var autoprefixer = require('autoprefixer');
  
-gulp.task('default', function () {
+gulp.task('css', function () {
 	return gulp.src('./css/*.styl')
 		.pipe(stylus())
 		.pipe(postcss([autoprefixer]))
 		.pipe(gulp.dest('./dist/css/'));
 });
+
+gulp.task('default', ['css']);
