031b70ca77e8c21065315e59cff1dd067813cdbe
Jacob Koperski <wijakoperski@gmail.com>
2017-12-30 23:03:24 -0600

    Added postcss and autoprefixer to node project

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,11 @@
 var gulp = require('gulp');
 var stylus = require('gulp-stylus');
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
  
 gulp.task('default', function () {
 	return gulp.src('./css/*.styl')
 		.pipe(stylus())
+		.pipe(postcss([autoprefixer]))
 		.pipe(gulp.dest('./dist/css/'));
 });
