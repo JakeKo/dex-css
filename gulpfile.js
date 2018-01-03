27e05c74ab912eaa1d65fbec6eb4e7dfe9184093
Jacob Koperski <wijakoperski@gmail.com>
2018-01-03 13:36:28 -0600

    Removed rename module

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,6 @@ var postcss = require('gulp-postcss');
 var autoprefixer = require('autoprefixer');
 var cssnano = require('cssnano');
 var mqpacker = require('css-mqpacker');
-var rename = require('gulp-rename');
  
 gulp.task('stylus', function () {
 	var processors = [
@@ -16,7 +15,6 @@ gulp.task('stylus', function () {
 	return gulp.src('./src/styl/*.styl')
 		.pipe(stylus())
 		.pipe(postcss(processors))
-		.pipe(rename({ extname: '.min.css' }))
 		.pipe(gulp.dest('./dist/css/'));
 });
 
